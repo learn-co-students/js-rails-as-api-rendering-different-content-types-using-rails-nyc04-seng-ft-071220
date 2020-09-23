e0c932ef23a1373f837498af78961c5234f4669a
wlcreate <waverley.nyc@gmail.com>
2020-09-23 19:34:49 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,8 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: @birds
+    render json: birds
+    # render json: @birds
     # render 'birds/index.html.erb'
   end
 end
